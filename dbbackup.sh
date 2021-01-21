dc1ff213307bb455fc9aefc8e94c5f240fa29e5b
cyweap <56427674+cyweap@users.noreply.github.com>
2021-01-21 15:58:41 +0530

    test
    test

--- dbbackup.sh
+++ dbbackup.sh
@@ -14,7 +14,14 @@ if [ -d /dbbackup/"$(date +%d%m%Y)" ];
 		mkdir /dbbackup/"$(date +%d%m%Y)"
 		echo "$(date +%d%m%Y) Directory Created"
 fi
-mysql -N -e 'show databases' | while read dbname; do mysqldump --complete-insert --routines --triggers --single-transaction "$dbname" | gzip > /dbbackup/"$(date +%d%m%Y)"/"$dbname""$(date +%d%m%Y)".sql.gz; done
+if [ -d /usr/sbin/plesk ];
+	then
+		echo "This is Plesk"
+		plesk db -e 'show databases' | while read dbname; do plesk db dump --complete-insert --routines --triggers --single-transaction "$dbname" | gzip > /dbbackup/"$(date +%d%m%Y)"/"$dbname""$(date +%d%m%Y)".sql.gz; done
+	else
+		echo "This is Cpanel/Linux"
+		mysql -N -e 'show databases' | while read dbname; do mysqldump --complete-insert --routines --triggers --single-transaction "$dbname" | gzip > /dbbackup/"$(date +%d%m%Y)"/"$dbname""$(date +%d%m%Y)".sql.gz; done
+fi
 
 find /dbbackup/ -type d -mtime +7 -print
 find /dbbackup/ -type d -mtime +7 -delete
