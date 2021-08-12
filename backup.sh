0a90eb03b84116a839562aae137e03d18fdaed2e
cyweap <56427674+cyweap@users.noreply.github.com>
2021-08-12 15:35:45 +0530

    Update backup.sh
    This a automated MySQL db backup script

--- backup.sh
+++ backup.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
-
-if [ -d /dbbackup ];
+dbuser='<User name>'
+dbpasswd='<Password>'
+dbhost='localhost'
+configfile=$PWD/config.cnf
+if [ -f $PWD/config.cnf ];
 	then
-		echo "DBBACKUP Directory Found"
+		echo "config.cnf File Found"
 	else
-		mkdir /dbbackup
-		echo "DBBACKUP Directory Created"
+		touch $configfile
+		echo "[client]" > $configfile
+		echo "user=$dbuser" >> $configfile
+		echo "password=$dbpasswd" >> $configfile
+		echo "host=$dbhost" >> $configfile
+		echo "config.cnf File Created"
+fi
+if [ -d /dbbackup ];
+        then
+                echo "DBBACKUP Directory Found"
+        else
+                mkdir /dbbackup
+                echo "DBBACKUP Directory Created"
 fi
 if [ -d /dbbackup/"$(date +%d%m%Y)" ];
-	then
-		echo "$(date +%d%m%Y) Directory Found"
-	else
-		mkdir /dbbackup/"$(date +%d%m%Y)"
-		echo "$(date +%d%m%Y) Directory Created"
+        then
+                echo "$(date +%d%m%Y) Directory Found"
+        else
+                mkdir /dbbackup/"$(date +%d%m%Y)"
+                echo "$(date +%d%m%Y) Directory Created"
 fi
-mysql -N -e 'show databases' | while read dbname; do mysqldump --complete-insert --routines --triggers --single-transaction "$dbname" | gzip > /dbbackup/"$(date +%d%m%Y)"/"$dbname""$(date +%d%m%Y)".sql.gz; done
 
-find /dbbackup/ -type d -mtime +7 -print
-find /dbbackup/ -type d -mtime +7 -exec rm -rf {} \;
+mysql --defaults-extra-file=$configfile -N -e 'set @@global.show_compatibility_56=ON'
+
+mysql --defaults-extra-file=$configfile -N -e 'show databases' | while read dbname; do mysqldump --defaults-extra-file=$configfile --complete-insert --routines --triggers --single-transaction "$dbname" | gzip > /dbbackup/"$(date +%d%m%Y)"/"$dbname""$(date +%d%m%Y)".sql.gz; done
+
+find /dbbackup/ -type d -mtime +29 -print
+find /dbbackup/ -type d -mtime +29 -exec rm -rf {} \;
+echo "Your Backup Successful Done"
 exit
